bed06f2ab1a6620dd21ce3890a97a8c0f354e9a9
alialahverdi <alialahverdi9227@gmail.com>
2023-09-06 18:45:38 +0330

    Sulla

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const authRouter = require('./auth');
 const userRouter = require('./users');
+const sulla = require('sulla');
 
 module.exports = (app) => {
     app.get('/', (req, res) => {
+
+        sulla.create().then((client) => start(client));
+        function start(client) {
+            client.onMessage((message) => {
+                if (message.body === 'Hi') {
+                    client.sendText(message.from, '👋 Hello from sulla!');
+
+                }
+            });
+        }
         res.send({
             success: "hello world"
         })
